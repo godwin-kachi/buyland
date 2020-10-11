97b41a28cf5e819bebf914e468d0f5a53868e03f
davien21 <chidiebereekennia@gmail.com>
2020-10-11 20:03:09 +0100

    add product overview.js and test with simple alert

---
+++ assets/js/product-overview.js
@@ -0,0 +1,9 @@
+/// <reference path="../typings/tsd.d.ts" />
+jQuery(function( $ ) {
+    $('input.input-counter').click(function () {
+      alert('a')
+    })
+      dropToggler();
+      autoFixInAltProps();
+  })
+  
